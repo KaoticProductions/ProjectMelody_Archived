047b60843833628ed679f9fff18e215718686fa9
aklein <aaronrklein@gmail.com>
2016-05-19 21:56:25 -0700

    adding pictures and updating base

---
+++ melody-base/melody-client/src/components/Test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FaBeer from 'react-icons/lib/fa/beer';
+
+export default React.createClass({
+  render: function() {
+    return <h3> Lets go for a <FaBeer />? </h3>
+  }
+});
